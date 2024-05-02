f95ba6a01804c532059df596c097b2f9086e35e4
Dev-Duardo <sp.e@outlook.com>
2024-05-02 17:47:00 -0300

    anatomia das classes

---
+++ src/AnatomiaDasClases.java
@@ -0,0 +1,28 @@
+public class AnatomiaDasClases {
+    public static void main(String[] args) {
+        System.out.println("Anatomia das clases \n void: vai executar e não vai retornar nada.\n Nome da Variável: primeira letra minuscula e as proximas palavras com letra maiuscula. Ex: anoFrabricacao ");
+        //variável com as letras todas maisculas, não podem ser alteradas, são do tipo final
+
+        //final string BR ="Brasil", variavel que não deve ser alterada.;
+        String BR = "Brasil";
+        int ano = 2023;
+        ano = 2024;
+        System.out.println(ano);
+        System.out.println(BR);
+
+        //retorna uma String, mas recebe um numerico
+        //String formatrCep (long cep);
+    }
+
+    public static void main2(String[] args) {
+        String primeiroNome = "Eduardo";
+        String segundoNome = "Pires";
+
+        String nomeCompleto = nomeCompleto (primeiroNome, segundoNome);
+    }
+
+    public static String nomeCompleto(String primeiroNome, String segundoNome) {
+        return primeiroNome.concat(" ").concat(segundoNome);
+    }
+    
+}
